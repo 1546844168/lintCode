171b3bd1df1a8bf9d063b31953e379853e4b13f5
江峰 <jiangfeng@smarchit.com>
2019-11-27 11:14:47 +0800

    删除排序数组中的重复项

--- src/Solution26.java
+++ src/Solution26.java
@@ -1,6 +1,33 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
+ * 26. 删除排序数组中的重复项
+ *
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 示例 1:
+ *
+ * 给定数组 nums = [1,1,2],
+ *
+ * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 示例 2:
+ *
+ * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+ *
+ * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author 江峰
  * @create 2019-11-26   13:42
  */
@@ -48,7 +75,7 @@ public class Solution26 {
     }
 
     public static void main(String[] args) {
-        int[] nums = getOrderArray(100, 100);
+        int[] nums = Utils.getOrderArray(100, 100);
         System.out.println(Arrays.toString(nums));
         System.out.println("length = " + nums.length);
         // int count = removeDuplicates(nums);
@@ -60,28 +87,5 @@ public class Solution26 {
         System.out.println("count = " + count);
         System.out.println("set.size = " + set.size());
         System.out.println(Arrays.toString(nums));
-    }
-
-    /**
-     * 获取随机元素有序的int数组
-     *
-     * @param length 数组的长度
-     * @param maxNum 数组里面最大的数
-     * @return
-     */
-    public static int[] getOrderArray(int length, int maxNum) {
-        Random random = new Random();
-        List<Integer> integers = new ArrayList<Integer>();
-        for (int i = 0; i < length; i++) {
-            integers.add(random.nextInt(maxNum));
-        }
-        Collections.sort(integers);
-        Integer[] nums = new Integer[integers.size()];
-        nums = integers.toArray(nums);
-        int[] ints = new int[integers.size()];
-        for (int i = 0; i < nums.length; i++) {
-            ints[i] = nums[i];
-        }
-        return ints;
     }
 }
