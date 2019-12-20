d277b232924e54d7266127c04e8e2f540d251c38
江峰 <jiangfeng@smarchit.com>
2019-12-20 11:12:15 +0800

    合并两个有序链表

---
+++ src/Solution21.java
@@ -0,0 +1,74 @@
+/**
+ * 21. 合并两个有序链表
+ * <p>
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author 江峰
+ * @create 2019-12-06   10:40
+ */
+public class Solution21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                curr = curr.next;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                curr = curr.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 == null) {
+            curr.next = l2;
+        } else {
+            curr.next = l1;
+        }
+        return head.next;
+
+        /*
+            // 递归：代码更优雅
+            if (l1 == null) {
+                return l2;
+            }
+            if (l2 == null) {
+                return l1;
+            }
+
+            if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        */
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l4 = new ListNode(4);
+        l1.next = l2;
+        l1.next.next = l4;
+        System.out.println("---------");
+        ListNode l5 = new ListNode(1);
+        ListNode l6 = new ListNode(3);
+        ListNode l7 = new ListNode(4);
+        l5.next = l6;
+        l5.next.next = l7;
+        ListNode listNode = new Solution21().mergeTwoLists(l1, l5);
+        System.out.println(listNode);
+    }
+}
