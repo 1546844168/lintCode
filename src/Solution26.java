c9896a637a7137a8621bf64d73f3786a5b1f576c
江峰 <jiangfeng@smarchit.com>
2020-04-08 14:55:41 +0800

    26. 删除排序数组中的重复项

--- src/Solution26.java
+++ src/Solution26.java
@@ -4,26 +4,26 @@ import java.util.Set;
 
 /**
  * 26. 删除排序数组中的重复项
- *
+ * <p>
  * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
- *
+ * <p>
  * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 给定数组 nums = [1,1,2],
- *
+ * <p>
  * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
- *
+ * <p>
  * 你不需要考虑数组中超出新长度后面的元素。
  * 示例 2:
- *
+ * <p>
  * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
- *
+ * <p>
  * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
- *
+ * <p>
  * 你不需要考虑数组中超出新长度后面的元素。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -78,8 +78,7 @@ public class Solution26 {
         int[] nums = Utils.getOrderArray(100, 100);
         System.out.println(Arrays.toString(nums));
         System.out.println("length = " + nums.length);
-        // int count = removeDuplicates(nums);
-        int count = removeDuplicates1(nums);
+        int count = removeDuplicates(nums);
         Set<Integer> set = new HashSet<Integer>();
         for (int num : nums) {
             set.add(num);
