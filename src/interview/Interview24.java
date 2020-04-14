f384738a223ded1eba6ccd254cf232e0a7064e0d
江峰 <jiangfeng@smarchit.com>
2020-04-14 11:02:43 +0800

    面试题24. 反转链表

---
+++ src/interview/Interview24.java
@@ -0,0 +1,51 @@
+package interview;
+
+import base.ListNode;
+
+/**
+ * 面试题24. 反转链表
+ * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+ * <p>
+ * <p>
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * <p>
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 节点个数 <= 5000
+ *
+ * @author 江峰
+ * @create 2020-04-14   10:32
+ */
+public class Interview24 {
+    /**
+     * 我们可以申请两个指针，第一个指针叫 pre，最初是指向 null 的。
+     * 第二个指针 cur 指向 head，然后不断遍历 cur。
+     * 每次迭代到 cur，都将 cur 的 next 指向 pre，然后 pre 和 cur 前进一位。
+     * 都迭代完了(cur 变成 null 了)，pre 就是最后一个节点了。
+     * 动画演示如下：
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+//申请节点，pre和 cur，pre指向null
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode tmp = null;
+        while (cur != null) {
+            //记录当前节点的下一个节点
+            tmp = cur.next;
+            //然后将当前节点指向pre
+            cur.next = pre;
+            //pre和cur节点都前进一位
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+}
