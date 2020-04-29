ae954b7df01d312b79db7dfc0bc114991ef40da7
江峰 <jiangfeng@smarchit.com>
2020-04-29 19:07:36 +0800

    面试题04. 二维数组中的查找

---
+++ src/interview/Interview04.java
@@ -0,0 +1,74 @@
+package interview;
+
+/**
+ * 面试题04. 二维数组中的查找
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * <p>
+ * <p>
+ * <p>
+ * 示例:
+ * <p>
+ * 现有矩阵 matrix 如下：
+ * <p>
+ * [
+ * [1,   4,  7, 11, 15],
+ * [2,   5,  8, 12, 19],
+ * [3,   6,  9, 16, 22],
+ * [10, 13, 14, 17, 24],
+ * [18, 21, 23, 26, 30]
+ * ]
+ * 给定 target = 5，返回 true。
+ * <p>
+ * 给定 target = 20，返回 false。
+ * <p>
+ * <p>
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= n <= 1000
+ * <p>
+ * 0 <= m <= 1000
+ *
+ * @author 江峰
+ * @create 2020-04-29   18:56
+ */
+public class Interview04 {
+    /**
+     * 时间复杂度 O(n + m)
+     * 空间复杂度 O(1)
+     *
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length;
+        int row = 0;
+        int column = matrix[0].length - 1;
+        while (row < rows && column >= 0) {
+            int num = matrix[row][column];
+            if (num == target) {
+                return true;
+            } else if (num > target) {
+                column--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] nums = {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        System.out.println(new Interview04().findNumberIn2DArray(nums, 20));
+    }
+}
